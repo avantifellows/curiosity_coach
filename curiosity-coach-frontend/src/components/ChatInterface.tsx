52537edbb325651b23e5f0123e142a60b9006879
suryabulusu <teja.surya59@gmail.com>
2025-05-24 01:24:35 +0530

    eslint fix

--- curiosity-coach-frontend/src/components/ChatInterface.tsx
+++ curiosity-coach-frontend/src/components/ChatInterface.tsx
@@ -25,7 +25,6 @@ const ChatInterface: React.FC = () => {
     isLoadingBrainConfig,
     brainConfigError,
     fetchBrainConfigSchema,
-    handleCreateConversation,
     conversations,
     isLoadingConversations,
   } = useChat();
